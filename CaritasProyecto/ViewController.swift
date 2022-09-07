efadc2b09b84934a7a480d197bb865400019357e
jaiovi <sebastianjo2011@gmail.com>
2022-09-06 22:53:12 -0500

    Creación paleta de color y pagina inicial con constraints listas

--- CaritasProyecto/ViewController.swift
+++ CaritasProyecto/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfEmail: UITextField!
+    @IBOutlet weak var tfPass: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
